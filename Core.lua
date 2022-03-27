4a0938f35161909210c85c7fd117aa4c6beeae42
Sumsebrum <kyle@xicon.de>
2022-03-27 21:15:06 +0200

    fix lua error

--- Core.lua
+++ Core.lua
@@ -28,6 +28,8 @@ local PREFIX = "TotemPlates v"
 local MAJOR, MINOR = "TotemPlates", 1
 local Core = LibStub:NewLibrary(MAJOR, MINOR)
 local L
+Core.debug = false
+
 Core.version_major_num = 1
 Core.version_minor_num = 0.00
 Core.version_num = Core.version_major_num + Core.version_minor_num
@@ -255,7 +257,7 @@ function Core:OnProfileChanged()
 end
 
 function Core:OnEnable()
-    self:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
+    --self:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
     self:RegisterEvent("PLAYER_ENTERING_WORLD")
     self:RegisterEvent("PLAYER_REGEN_ENABLED")
 end
@@ -292,7 +294,7 @@ function Core:PLAYER_ENTERING_WORLD()
 
 end
 
-function Core:UPDATE_BATTLEFIELD_STATUS(_, index)
+function Core:UPDATE_BATTLEFIELD_STATUS(index)
     local status, mapName, instanceID, levelRangeMin, levelRangeMax, teamSize, isRankedArena, suspendedQueue, bool, queueType = GetBattlefieldStatus(index)
     local instanceType = select(2, IsInInstance())
     Core:Debug("INFO", "UPDATE_BATTLEFIELD_STATUS", instanceType, status, teamSize)
